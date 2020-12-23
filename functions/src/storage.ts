66b54ac669a2c7a4458369fc897e9a7bb628a416
Poom Chantarapornrat <poomchantara@gmail.com>
2020-12-23 13:28:16 +0700

    clean code

--- functions/src/storage.ts
+++ functions/src/storage.ts
@@ -5,28 +5,33 @@ export const onStoreUpdate = functions.firestore
   .document(`/stores/{storeId}`)
   .onUpdate(async (change, context) => {
     const storeId = context.params.storeId;
+    const bucket = admin.storage().bucket();
     const before = change.before.data();
     const after = change.after.data();
     if (before == after) return;
-    const bucket = admin.storage().bucket();
 
+    // clean the banner in storage
     if (before[`bannerUrl`] != after[`bannerUrl`]) {
-      const url: string = before['bannerUrl'];
-      const imgId = url.split(`img_`).pop()!.split(`?`)[0];
+      const imgId = getIdFromUrl(before['bannerUrl']);
       const path = `stores/store_${storeId}/banner/img_${imgId}`;
       await bucket.file(path).delete();
     }
 
+    // clean the pics in storage
     const prevList: Array<string> = before['picUrls'];
     const newList: Array<string> = after['picUrls'];
     if (newList != prevList) {
       for (let url of prevList) {
         if (!newList.includes(url)) {
-          const imgId = url.split(`img_`).pop()!.split(`?`)[0];
+          const imgId = getIdFromUrl(url);
           const path = `stores/store_${storeId}/pics/img_${imgId}`;
           await bucket.file(path).delete();
         }
       }
+    }
+    
+    function getIdFromUrl(url : string) : string {
+      return url.split(`img_`).pop()!.split(`?`)[0];
     }
 
   })
